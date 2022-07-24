587f3a3710699a52d3dcd91af81aa2403ff71a34
FerminLarranaga <fermin.larranaga05@gmail.com>
2022-07-24 17:39:19 -0300

    Improvements

--- scripts.js
+++ scripts.js
@@ -1,29 +1,43 @@
 let currentVal = 0;
 let cardsLeft = 2*52;
 let realVal = 0;
-let currentValP = document.getElementById('currentVal');
 let realValP = document.getElementById('realVal');
 let cardsLeftP = document.getElementById("cardsLeft");
+let altasP = document.getElementById("altas");
+let mediasP = document.getElementById("medias");
+let bajasP = document.getElementById("bajas");
+const cards = {
+    altas: cardsLeft/52*20,
+    medias: cardsLeft/52*12,
+    bajas: cardsLeft/52*20,
+}
+updateVals();
 
 function addOne () {
-    console.log('hi')
     currentVal++;
     cardsLeft--;
+    cards['bajas'] = cards.bajas-1;
     updateVals();
 }
-function addZero(){
+
+function addZero () {
     cardsLeft--;
+    cards['medias'] = cards.medias-1;
     updateVals();
 }
+
 function substractOne(){
     currentVal--;
     cardsLeft--;
+    cards['altas'] = cards.altas-1;
     updateVals();
 }
 
 function updateVals(){
-    realVal = (currentVal/(cardsLeft/52).toFixed(3)).toFixed(3);
-    currentValP.innerText = "currentVal: " + currentVal;
-    realValP.innerText = "realVal: " + realVal;
-    cardsLeftP.innerText = "Cards Left: " + cardsLeft;
+    realVal = (currentVal/(cardsLeft/52).toFixed(3)).toFixed(1);
+    realValP.innerText = realVal;
+    cardsLeftP.innerText = cardsLeft;
+    altasP.innerText = cards.altas;
+    mediasP.innerText = cards.medias;
+    bajasP.innerText = cards.bajas;
 }
